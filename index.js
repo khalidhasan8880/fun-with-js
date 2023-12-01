c273190cce63ded272e137b991f6245766989f46
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-12-01 17:41:31 +0600

    added send auto message

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+
+let counter = 1;
+function callMyFunc(params) {
+    document.getElementById('composerInput').innerText = counter
+    document.querySelector('[value="Send"]').click()
+    counter++
+}
+ setInterval(() => {
+   if (counter < 5) {
+    callMyFunc()
+   }    
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Cancel all friend requests with one click.
-
-document.querySelectorAll('[aria-level="Cancel request"]').forEach(x=>x.click())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// document.querySelectorAll('[aria-level="Cancel request"]').forEach(x=>x.click())
 
 
 
@@ -25,13 +40,9 @@ document.querySelectorAll('[aria-level="Cancel request"]').forEach(x=>x.click())
 // doc.querySelectorAll('[data-visualcompletion="ignore-dynamic"] div a').forEach(element => {
 //   names.push(element.textContent.trim());
 // });
-// chatGPT : give me only girls names in an array :
 
-
-// second step 
-
-// const doc = document.querySelector('[aria-labelledby=":r29n:"]')
-// doc.querySelectorAll('[data-visualcompletion="ignore-dynamic"] div').forEach(element => {
+// const mainBody = document.querySelector('[aria-labelledby=":r29n:"]')
+// mainBody.querySelectorAll('[data-visualcompletion="ignore-dynamic"] div').forEach(element => {
 //   // console.log()
 // if (onlyGirls.includes(element.querySelector('a')?.textContent.trim())) {
 //     element.querySelector('[aria-label="Add friend"]')?.click()
